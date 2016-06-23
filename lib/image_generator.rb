9cbb6021ed34a5edcedee1d71aa3c50bff07d9ee
matugm <matugm@gmail.com>
2016-06-23 22:57:58 +0200

    Add image generator class.

---
+++ lib/image_generator.rb
@@ -0,0 +1,34 @@
+
+class ImageGenerator
+  def self.generate(header, data)
+    output = File.open("#{__dir__}/../img/original.png", "wb")
+
+    ## Write PNG signature + IHDR ##
+
+    header.write(output)
+
+    ## Write data chunk ##
+
+    Chunk.new.tap do |ch|
+      ch.data = data
+      ch.crc  = Zlib.crc32("IDAT" + ch.data)
+
+      ch.chunk_length = ch.data.size
+      ch.chunk_type   = 1229209940
+
+      ch.write(output)
+    end
+
+    ## Write IEND chunk ##
+
+    Chunk.new.tap do |ch|
+      ch.data = ""
+      ch.crc  = Zlib.crc32("IEND")
+
+      ch.chunk_length = 0
+      ch.chunk_type   = 1229278788
+
+      ch.write(output)
+    end
+  end
+end
