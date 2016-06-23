dcde3546de6e80e1c51700d3448d52a527f84653
matugm <matugm@gmail.com>
2016-06-23 23:00:18 +0200

    Add example images.

--- app.rb
+++ app.rb
@@ -5,22 +5,29 @@ require_relative 'lib/chunk'
 require_relative 'lib/png-header'
 require_relative 'lib/compressor'
 require_relative 'lib/filter_remover'
+require_relative 'lib/filter_adder'
+require_relative 'lib/image_generator'
 
-ARGV[0] = "/home/matu/Desktop/test-image.png"
 
-abort "Please provide a file name." unless ARGV[0]
+if !ARGV[0]
+  file_name = "#{__dir__}/img/original.png"
+else
+  file_name = ARGV[0]
+end
 
-f = File.open(ARGV[0])
-pp header = PNG_Image.read(f)
+file      = File.open(file_name)
+pp header = PNG_Image.read(file)
 
 chunks = []
 
-loop { chunks << Chunk.read(f); pp chunks.last; break if chunks.last.chunk_type == 1229278788 }
+loop { chunks << Chunk.read(file); pp chunks.last; break if chunks.last.chunk_type == 1229278788 }
 
 image_data = Compressor.inflate(chunks)
 pixels     = FilterRemover.unfilter(image_data, header)
+
 # Stegano class
-image_data = FilterAdder.filter(pixels)
+
+image_data = FilterAdder.filter(pixels, header)
 compressed = Compressor.deflate(image_data)
 
-generate_image(header, compressed)
+ImageGenerator.generate(header, compressed)
